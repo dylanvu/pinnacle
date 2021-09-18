5029e479fbe14c0181884f73d47255ef486ab095
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-09-17 21:02:07 -0500

    dv - implemented mouse based smooth lines

---
+++ game.py
@@ -0,0 +1,43 @@
+import pygame
+from gameFuncts import *
+
+# Set the pygame window size
+SCREEN_HEIGHT = 1000
+SCREEN_WIDTH = 1850
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+BACKGROUND_COLOR = pygame.Color(251, 251, 248)
+BLACK = pygame.Color(0, 0, 0)
+
+# Intialize all relevant variables for the game
+running = True
+mouse = pygame.mouse.get_pos()
+prevMouse = pygame.mouse.get_pos()
+
+screen.fill(BACKGROUND_COLOR)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+
+    # Check for mouse down and draw if pressed
+    if pygame.mouse.get_pressed()[0]:
+        InterpolatePoints(screen, BLACK, prevMouse[0], prevMouse[1], mouse[0], mouse[1], 5)
+
+    # Once we receive coordinates:
+    # if (existsCoordinates) and (draw):
+    #     DrawDot(screen, BLACK, x coord, y coord, 10)
+
+    # if (existsCoordinates) and (erase):
+    #     DrawDot(screen, ERASE COLOR, x coord, y coord, 10)
+
+    # Save previous mouse and get new mouse for interpolation of points
+    prevMouse = mouse
+    mouse = pygame.mouse.get_pos()
+
+    # Flip the display
+    pygame.display.flip()
+
---
+++ gameFuncts.py
@@ -0,0 +1,19 @@
+import pygame
+import math
+
+def DrawDot(screen, color, x, y, radius):
+    # Basic function for drawing a circle with parameters
+    pygame.draw.circle(screen, color, (x, y), radius)
+
+def InterpolatePoints(screen, color, x1, y1, x2, y2, thickness):
+    pygame.draw.aaline(screen, color, (x1, y1), (x2, y2))
+
+    # Deal with the larger width
+    for num in range(thickness):
+        pygame.draw.aaline(screen, color, (x1 + num, y1), (x2 + num, y2))
+        pygame.draw.aaline(screen, color, (x1 - num, y1), (x2 - num, y2))
+        pygame.draw.aaline(screen, color, (x1, y1 + num), (x2, y2 + num))
+        pygame.draw.aaline(screen, color, (x1, y1 - num), (x2, y2 - num))
+
+    # Maybe: create circles instead? Make smoother on edges of interpolation
+    # Check if we're moving to the left/right and up/down and don't draw depending on the direction
