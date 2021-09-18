c1efdadb308fdf9e9ed2d5eb1659f146af9ee73f
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-09-18 11:55:21 -0500

    dv - ported calibration to rpi

--- calibration/pictures.py
+++ calibration/pictures.py
@@ -1,9 +1,22 @@
 import cv2
+import time
 
-cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop) 
+# cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop) 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+camera = PiCamera()
+camSize = (640, 480)
+camera.resolution = camSize
+camera.framerate = 32
+
+rawCapture = PiRGBArray(camera, size=camSize)
+
+time.sleep(0.1)
+
 imgCount = 0 # Number of images
 
-while(True):
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     ret,frame = cap.read() # return a single frame in variable `frame`
     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('Capturing Video',frame) #display the captured image
@@ -14,5 +27,19 @@ while(True):
     if key == ord('c'): # save picture on pressing 'c' 
         cv2.imwrite(str(imgCount) + ".jpg",frame)
         imgCount += 1
+    rawCapture.truncate(0)
 
-cap.release()
+
+# while(True):
+#     ret,frame = cap.read() # return a single frame in variable `frame`
+#     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     cv2.imshow('Capturing Video',frame) #display the captured image
+#     key = cv2.waitKey(1) & 0xFF
+#     if key == ord('y'): # exit on pressing 'y' 
+#         cv2.destroyAllWindows()
+#         break
+#     if key == ord('c'): # save picture on pressing 'c' 
+#         cv2.imwrite(str(imgCount) + ".jpg",frame)
+#         imgCount += 1
+
+# cap.release()
