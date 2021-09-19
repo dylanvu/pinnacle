683c43afec0120f24b48a19b55d51cb4d3c98f1d
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-09-19 08:07:44 -0500

    Calibration done again

--- hardcodewarp.py
+++ hardcodewarp.py
@@ -7,7 +7,7 @@ def HardCorrectWarping(SCREEN_DIMS):
     correctWidth = SCREEN_DIMS[0]
     correctHeight = SCREEN_DIMS[1]
 
-    points1 = np.float32([[151, 50], [533, 35], [181, 275], [536, 252]])
+    points1 = np.float32([[156, 86], [510, 94], [140, 287], [501, 300]])
     points2 = np.float32([[0,0], [correctWidth, 0], [0, correctHeight], [correctWidth,correctHeight]])
 
     correctedMatrix = cv2.getPerspectiveTransform(points1, points2)
