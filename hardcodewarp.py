6f667dfd616ae718d0c66e1c31f144f42084236f
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-09-18 23:40:41 -0500

    fixed dimensions of warp

--- hardcodewarp.py
+++ hardcodewarp.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def HardCorrectWarping():
 
-    correctWidth, correctHeight = 1080,720
+    correctWidth, correctHeight = 540,360
 
     points1 = np.float32([[185, 109], [466, 130], [180, 270], [460, 285]])
     points2 = np.float32([[0,0], [correctWidth, 0], [0, correctHeight], [correctWidth,correctHeight]])
