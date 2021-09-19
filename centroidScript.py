75c7e8ad5e4ca285fe3ad3df59fa9034f08f8eb6
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-09-19 07:54:04 -0500

    added cnt instead of cnts

--- centroidScript.py
+++ centroidScript.py
@@ -34,7 +34,7 @@ def centroidScript(frame):
         cnts, _ = cv2.findContours(res, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         if len(cnts) > 0:
             cnt = sorted(cnts, key=cv2.contourArea)
-            c1 = cnts[0]
+            c1 = cnt[0]
             (cX,cY),radius = cv2.minEnclosingCircle(c1)
             cX = int(cX)
             cY = int(cY)
