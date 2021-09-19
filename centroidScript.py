0c58b24b696c1a6a7556e76a958333291b3b0876
mrdevlin6 <mrdevlin6@gmail.com>
2021-09-18 19:37:17 -0500

    updated red and green channel thresholds

--- centroidScript.py
+++ centroidScript.py
@@ -2,9 +2,9 @@ import numpy as np
 import cv2
 import time
 #will output x,y,colorString
-def centroidScript(frame):
-    lower_color_green = (10, 100, 35) #BGR
-    upper_color_green = (35,255,90)
+def centroidScript():
+    lower_color_green = (0, 90, 20)
+    upper_color_green = (90,255,135)
     maskGreen = cv2.inRange(frame,lower_color_green,upper_color_green)
     res1 = cv2.bitwise_and(frame,frame, mask= maskGreen)
 
@@ -12,10 +12,11 @@ def centroidScript(frame):
     imgGray = cv2.cvtColor(mask_rgb, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(imgGray, 200, 255, cv2.THRESH_BINARY)
     countG = cv2.countNonZero(thresh)
+    #
+    # countG =0
 
-
-    lower_color_red = (40, 0, 180)
-    upper_color_red = (104, 83,220)
+    lower_color_red = (40, 15, 110) #180
+    upper_color_red = (90, 70,255)
     maskRed = cv2.inRange(frame,lower_color_red,upper_color_red)
     res2 = cv2.bitwise_and(frame,frame, mask= maskRed)
     mask_rgb = cv2.cvtColor(maskRed,cv2.COLOR_GRAY2BGR)
@@ -23,28 +24,30 @@ def centroidScript(frame):
     _, thresh1 = cv2.threshold(imgGray1, 200, 255, cv2.THRESH_BINARY)
 
     countR = cv2.countNonZero(thresh1)
+    # print(countR)
 
     res = cv2.bitwise_or(thresh,thresh1)
     cX = -1
     cY = -1
-    # cv2.imshow("res",thresh)
+    # cv2.imshow("res",res) TODO
     try:
         cnts, hierarchy = cv2.findContours(res, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        if len(cnts)> 1:
+        if len(cnts) > 1:
             cnt = sorted(cnts, key=cv2.contourArea)
             c1 = cnts[0]
             (cX,cY),radius = cv2.minEnclosingCircle(c1)
             cX = int(cX)
             cY = int(cY)
             radius = int(radius)
-            cv2.drawContours(frame, c1, -1, (255, 0, 0), 1)
+            # cv2.drawContours(frame, c1, -1, (255, 0, 0), 5) TODO
+        
     except:
         cX = -1
         cY = -1
     # print(countG,countR)
     if countG > 1:
-        return((cX,cY),"green")
+        return(cX,cY,"green")
     elif countR > 1:
-        return((cX,cY),"red")
+        return(cX,cY,"red")
     else:
         return((None,None),None)
